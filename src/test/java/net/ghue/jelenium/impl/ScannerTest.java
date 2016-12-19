b303ceb71df4668acaa478914c2afb3cc5ca16a0
Luke Last <luke@lukelast.com>
2016-12-19 02:39:45 -0500

    Fix warning

--- src/test/java/net/ghue/jelenium/impl/ScannerTest.java
+++ src/test/java/net/ghue/jelenium/impl/ScannerTest.java
@@ -6,7 +6,6 @@ import net.ghue.jelenium.demo.SampleTest;
 
 public class ScannerTest {
 
-   @SuppressWarnings( "unchecked" )
    @Test
    public void testFindTests() throws Exception {
       Assertions.assertThat( Scanner.findTests() ).contains( SampleTest.class );
