c39fa521d306c17017730fb21631099ef996b488
Luke Last <luke@lukelast.com>
2020-12-12 15:05:59 -0500

    Create JeleniumRunnerBuilder
    This contains the code needed to setup the JeleniumRunner and will allow future customization.

---
+++ src/main/java/net/ghue/jelenium/JeleniumRunnerBuilder.java
@@ -0,0 +1,30 @@
+package net.ghue.jelenium;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import net.ghue.jelenium.impl.GuiceModule;
+import net.ghue.jelenium.impl.JeleniumRunner;
+
+public final class JeleniumRunnerBuilder {
+
+   @Nullable
+   private String[] args;
+
+   public JeleniumRunnerBuilder withArgs( String[] newArgs ) {
+      this.args = newArgs;
+      return this;
+   }
+
+   @Nonnull
+   public String[] getArgs() {
+      return ( args != null ) ? args : new String[0];
+   }
+
+   public JeleniumRunner build() {
+      final Injector injector = Guice.createInjector( new GuiceModule( getArgs() ) );
+      final JeleniumRunner jelenium = injector.getInstance( JeleniumRunner.class );
+      return jelenium;
+   }
+}
--- src/main/java/net/ghue/jelenium/Main.java
+++ src/main/java/net/ghue/jelenium/Main.java
@@ -1,12 +1,5 @@
 package net.ghue.jelenium;
 
-import java.util.Objects;
-import javax.annotation.Nullable;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import net.ghue.jelenium.impl.GuiceModule;
-import net.ghue.jelenium.impl.JeleniumRunner;
-
 /**
  * <p>
  * A test runner main class that can be used as an entry point to run SELENIUM tests.
@@ -24,11 +17,7 @@ public final class Main {
     * @param args Command line arguments.
     * @throws java.lang.Exception if any.
     */
-   public static void main( @Nullable String[] args ) throws Exception {
-      Objects.requireNonNull( args );
-
-      final Injector injector = Guice.createInjector( new GuiceModule( args ) );
-      final JeleniumRunner jelenium = injector.getInstance( JeleniumRunner.class );
-      jelenium.run();
+   public static void main( String[] args ) throws Exception {
+      new JeleniumRunnerBuilder().withArgs( args ).build().run();
    }
 }
